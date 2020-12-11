5f9f7f49b135f965150816b133acd9a43f0a9e38
yuriuno <yuriuno@icloud.com>
2020-12-11 22:01:03 +0900

    project name is changed from scaleheight_lifetime to scaleheight-lifetime

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open(path.join(here,'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='scaleheight_lifetime',
-    packages=['scaleheight_lifetime'], 
+    name='scaleheight-lifetime',
+    packages=['scaleheight-lifetime'], 
     version='1.0.0', 
     license='MIT', 
     install_requires=['numpy','scipy'], 
     author='yuriuno', 
     author_email='k8688309@kadai.jp',
-    url='https://github.com/yuriuno/scaleheight_lifetime',
+    url='https://github.com/yuriuno/scaleheight-lifetime',
     description='Estimate a distribution and lifetime of star in the Galaxy.',
     long_description=long_description, 
     long_description_content_type='text/markdown',
